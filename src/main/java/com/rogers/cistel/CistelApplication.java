bee1158f509a09aefb78cc254132447ee4e734e6
Rahul Kumar <rahul_sgr@live.in>
2020-07-04 21:57:06 +0530

    first commit

---
+++ src/main/java/com/rogers/cistel/CistelApplication.java
@@ -0,0 +1,13 @@
+package com.rogers.cistel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CistelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CistelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogers/cistel/CistelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogers.cistel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CistelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
